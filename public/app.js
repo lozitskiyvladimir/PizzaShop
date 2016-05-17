47ee5a6b923e17357c5ae3efb401fbbd40db0e58
Lozitskiy Vladimir <loza-w@mail.ru>
2016-05-17 17:10:55 +0300

    Add cart_get_number_of_items function

--- public/app.js
+++ public/app.js
@@ -4,6 +4,7 @@ function add_to_cart(id)
     var x = window.localStorage.getItem(key);
     x = x*1+1;
     window.localStorage.setItem(key, x);
+    alert('Items in your cart:' + cart_get_number_of_items());
 }
 
 
@@ -15,4 +16,21 @@ function something()
   x = x * 1 + 1;
   window.localStorage.setItem('bbb', x)
   alert(x);
+}
+
+function cart_get_number_of_items()
+{
+  var cnt = 0;
+  for (var i = 0; i < localStorage.length; i++)
+  {
+      var key = window.localStorage.key(i);
+      var value = window.localStorage.getItem(key);
+      if(key.indexOf('product_') == 0)
+      {
+        cnt = cnt + value*1;
+      }
+
+  }
+  return cnt;
+
 }
