620085297875b081e604ad07eec94d3006db3b7d
Lozitskiy Vladimir <loza-w@mail.ru>
2016-05-24 12:09:50 +0300

    Add comments to all function

--- public/app.js
+++ public/app.js
@@ -1,3 +1,8 @@
+
+// записать в localStorage 3 ключа  'product_1','product_2','product_3',и увеличивать их значение на 1
+//при нажатии на кнопку под каждой пицой
+
+
 function add_to_cart(id)
 {
     var key = 'product_' + id;
@@ -8,40 +13,7 @@ function add_to_cart(id)
     update_orders_button();
 }
 
-
-function something()
-
-{
-
-  var x = window.localStorage.getItem('bbb');
-  x = x * 1 + 1;
-  window.localStorage.setItem('bbb', x)
-  alert(x);
-}
-
-function update_orders_input()
-{
-  var orders = cart_get_orders();
-  $('#orders_input').val(orders);
-}
-
-function cart_get_number_of_items()
-{
-  var cnt = 0;
-  for (var i = 0; i < localStorage.length; i++)
-  {
-      var key = window.localStorage.key(i);
-      var value = window.localStorage.getItem(key);
-      if(key.indexOf('product_') == 0)
-      {
-        cnt = cnt + value*1;
-      }
-
-  }
-  return cnt;
-
-}
-
+// Создать из localStorage строку вида "'product_1' = 'count', 'product_2' = 'count', 'product_3 ' = 'count' "
 function cart_get_orders()
 {
   var orders = '';
@@ -59,27 +31,37 @@ function cart_get_orders()
 
 }
 
+//Передать строку в скрытое текстовое поле формы на главной странице,которая будет отправлять
+//ее на сервер
+function update_orders_input()
+{
+  var orders = cart_get_orders();
+  $('#orders_input').val(orders);
+}
+
+
+// Посчитать общее количество заказанных пиц
+function cart_get_number_of_items()
+{
+  var cnt = 0;
+  for (var i = 0; i < localStorage.length; i++)
+  {
+      var key = window.localStorage.key(i);
+      var value = window.localStorage.getItem(key);
+      if(key.indexOf('product_') == 0)
+      {
+        cnt = cnt + value*1;
+      }
+
+  }
+  return cnt;
+
+}
+
+
+//Вывести на кнопке корзины общее число заказанных пиц
 function update_orders_button()
 {
   var text = 'Cart('+ cart_get_number_of_items() + ')';
   $('#orders_button').val(text);
-}
-function get_peperoni_count()
-{
-  var key = 'product_2';
-  var peperoni_count = window.localStorage.getItem(key);
-  return peperoni_count;
-}
-
-function get_hawajjan_count()
-{
-  var key = 'product_1';
-  var hawajjan_count = window.localStorage.getItem(key);
-  return hawajjan_count;
-}
-function get_vegetarian_count()
-{
-  var key = 'product_3';
-  var vegetarian_count = window.localStorage.getItem(key);
-  return vegetarian_count;
 }
